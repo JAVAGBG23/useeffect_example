9e93fbee02ab6a1d9c3b1debe0a9e138e5a318b1
Helena <helena@klarr.se>
2024-05-07 11:10:30 +0200

    finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
-  const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState(
+    JSON.parse(localStorage.getItem("Todos")) || []
+  );
   const [inputValue, setInputValue] = useState("");
 
   const inputHandler = (e) => {
     setInputValue(e.target.value);
   };
+
+  const saveTodo = () => {
+    setTodos((prevState) => [
+      ...prevState,
+      {
+        text: inputValue,
+        id: uuidv4(),
+      },
+    ]);
+
+    setInputValue("");
+  };
+
+  const deleteTodo = (id) => {
+    const filteredTodos = todos.filter((todo) => todo.id !== id);
+    setTodos(filteredTodos);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("Todos", JSON.stringify(todos));
+  }, [todos]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem("Todos"));
+    if (data) {
+      setTodos(data);
+    }
+  }, []);
 
   return (
     <div className="container">
@@ -18,12 +48,17 @@ function App() {
           type="text"
           placeholder="Add a Todo"
         />
-        <button>+</button>
+        <button onClick={saveTodo}>+</button>
       </div>
-      <div className="todo-item">
-        <p className="title"></p>
-        <button className="todo-label">DELETE</button>
-      </div>
+
+      {todos.map((todo) => (
+        <div className="todo-item" key={todo.id}>
+          <p className="title">{todo.text}</p>
+          <button className="todo-label" onClick={() => deleteTodo(todo.id)}>
+            DELETE
+          </button>
+        </div>
+      ))}
     </div>
   );
 }
